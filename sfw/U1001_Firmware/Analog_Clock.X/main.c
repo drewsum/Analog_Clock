e35df8e1186143cef4b2e1e7c8716b87e2591af9
Drew <drewmaatman2gmail.com>
2020-10-14 20:54:17 -0500

    Created MPLABX Firmware Project

---
+++ sfw/U1001_Firmware/Analog_Clock.X/main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: Drew Maatman
+ *
+ * Created on October 14, 2020, 8:53 PM
+ */
+
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+void main(void) {
+    return;
+}
