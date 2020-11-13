2bfb63ec07aa06a70c09a9575f3a0498827f9cbb
Drew <drewmaatman2gmail.com>
2020-11-12 22:14:08 -0600

    MCU firmware version 1.0

--- sfw/U1001_Firmware/Analog_Clock.X/heartbeat_services.h
+++ sfw/U1001_Firmware/Analog_Clock.X/heartbeat_services.h
@@ -22,7 +22,7 @@
 
 #include "heartbeat_timer.h"
 
-#define FIRMWARE_VERSION_STR            "0.8"
+#define FIRMWARE_VERSION_STR            "1.0"
 
 // API Variables
 volatile uint32_t device_on_time_counter;
